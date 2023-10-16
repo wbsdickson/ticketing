288d4c9821d7ad43223847daff2c7486b28db653
Dickson Wu <wbs.dickson@gmail.com>
2023-10-15 21:21:11 -0600

    add prettier plugin

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,25 +1,36 @@
 "use client";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
-    return (
-        <div className=" w-screen h-screen pt-[80px] pb-[50px] overflow-auto" style={{ backgroundColor: "#ffebc8" }}>
-            <AspectRatio ratio={9 / 16}>
-                <img className="w-screen m-auto h-auto" src="/home-dummy.png" alt="" />
-            </AspectRatio>
-            <Card className="fixed right-10 bottom-4 bg-[#eca5b3] text-white drop-shadow-md border-0">
-                <CardHeader>
-                    <CardTitle>World Tour 2023</CardTitle>
-                    <CardDescription className="text-white">Only a few tickets left.</CardDescription>
-                </CardHeader>
-                <CardFooter>
-                    <Button variant="outline" className="text-black font-bold">
-                        Buy Now
-                    </Button>
-                </CardFooter>
-            </Card>
-        </div>
-    );
+  return (
+    <div
+      className=" h-screen w-screen overflow-auto pb-[50px] pt-[80px]"
+      style={{ backgroundColor: "#ffebc8" }}
+    >
+      <AspectRatio ratio={9 / 16}>
+        <img className="m-auto h-auto w-screen" src="/home-dummy.png" alt="" />
+      </AspectRatio>
+      <Card className="fixed bottom-4 right-10 border-0 bg-[#eca5b3] text-white drop-shadow-md">
+        <CardHeader>
+          <CardTitle>World Tour 2023</CardTitle>
+          <CardDescription className="text-white">
+            Only a few tickets left.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Button variant="outline" className="font-bold text-black">
+            Buy Now
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
